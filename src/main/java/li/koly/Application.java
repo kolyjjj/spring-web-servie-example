3f4f4a9a56fa0b9dac10052e66c9c509bf3f9787
kolyjjj <me.koly.li@gmail.com>
2015-11-23 14:52:34 +0800

    copy codes from spring web service tutorial and replage the namespace to li.koly

---
+++ src/main/java/li/koly/Application.java
@@ -0,0 +1,12 @@
+package li.koly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
